a23982686eb8e660ce4b5aecb992edd45e76dd43
Bojan <Maraucbojan@users.noreply.github.com>
2019-05-07 14:49:52 +0200

    Update main.js
    added javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// This adds logo in footer
+
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top -100
+      },
+      800
+    );
+  }
+});
